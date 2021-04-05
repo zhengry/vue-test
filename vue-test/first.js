a8bf56e560ee4f01009d1b3d3caa34acabf84475
Baylee <zrydev@gmail.com>
2021-04-05 11:31:36 +0800

    vue父子组件通信

---
+++ vue-test/first.js
@@ -0,0 +1,9 @@
+let a = 1
+console.log(a)
+
+const app = new Vue({
+  el: '.title1',
+  data: {
+    message: '臭臭'
+  }
+})
---
+++ vue-test/second.js
@@ -0,0 +1,6 @@
+const app1 = new Vue({
+  el: '.title2',
+  data: {
+    message1: '臭臭'
+  }
+})
